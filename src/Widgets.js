d771b2df28738e9b9633a3ffba94853731fcc1e4
gosrahul21 <gosrahul21@gmail.com>
2021-09-22 02:20:46 +0530

    deployment continues

--- src/Widgets.js
+++ src/Widgets.js
@@ -5,13 +5,14 @@ function Widgets() {
         <div className="widget">
             <iframe className="iframe" src="https://reactjs.org/docs/context.html" title="W3Schools Free Online Web Tutorials"
             width="340"
-            height="100%"
+            // height="100%"
             style={{border:"none"}}
             // scrolling = "no"
             frameBorder = "0"
             allowTransparency="true"
             allow = "encrypted-media"
             ></iframe>
+           <iframe width="340" height="315" src="https://www.youtube.com/embed/nthoDssMPSo" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         </div>
     )
 }
